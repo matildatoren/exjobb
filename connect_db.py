01513218e36ca54532a185f00d872abe681f6282
ma4210to-s@student.lu.se <matildatoren@gmail.com>
2026-02-09 09:49:39 +0100

    connection to db

---
+++ connect_db.py
@@ -0,0 +1,13 @@
+import os
+import psycopg2
+import dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_DSN = os.getenv("DB_URL")
+
+try:
+    conn = psycopg2.connect(DB_DSN)
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
