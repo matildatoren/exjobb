3d3c9199cc231c10d6b0d127a8a0089dbd73f476
ma4210to-s@student.lu.se <matildatoren@gmail.com>
2026-02-09 11:28:36 +0100

    more graphs added to the dashboard

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ st.set_page_config(page_title="Enkät-dashboard", layout="wide")
 conn = get_connection()
 
 @st.cache_data
+def load_entries_over_time():
+    query = """
+        SELECT
+            DATE(created_at) AS date,
+            COUNT(*) AS entries
+        FROM introductory
+        GROUP BY DATE(created_at)
+        ORDER BY date
+    """
+    return pd.read_sql(query, conn)
+
+
 def load_user_data():
     query = """
         SELECT *
@@ -45,14 +57,32 @@ def load_md_data():
     """
     return pd.read_sql(query, conn)
 
+def load_rt_data():
+    query = """
+        SELECT *
+        FROM refresh_tokens
+    """
+    return pd.read_sql(query, conn)
+
+df = load_entries_over_time()
 users = load_user_data()
 intro = load_intro_data()
 ht = load_ht_data()
 it = load_it_data()
 md = load_md_data()
+rt = load_rt_data()
+
 
 st.title("Survey Dashboard")
 
+st.metric("Totalt antal svar", len(intro))
+
+st.subheader("Antal entries per dag")
+st.line_chart(
+    df.set_index("date")["entries"]
+)
+
+st.header("All tables displayed")
 st.subheader("Users")
 st.dataframe(users)
 
@@ -67,3 +97,6 @@ st.dataframe(it)
 
 st.subheader("Motorical development")
 st.dataframe(md)
+
+st.subheader("Refresh tokens")
+st.dataframe(rt)
