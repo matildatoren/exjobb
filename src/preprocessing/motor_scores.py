6dd9afe3a7031b7503b50d3f7e7e71a8a5f2b76e
Maja Melms <ma2224me-s@student.lu.se>
2026-03-19 13:59:48 +0100

    lagt till importer

--- src/preprocessing/motor_scores.py
+++ src/preprocessing/motor_scores.py
@@ -1,5 +1,16 @@
+from preprocessing_md import extract_milestone_keys, count_milestones, sum_impairments
+
+import polars as pl
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+
 # normalierat över en satt vektor med "unlocked" milestones/impairments
 def motorscore_milestones_setvalue() -> float:
+    possible_milestones_by_age = {1: 12, 2: 19, 3: 25, 4: 31, 5: 36, 6: 39, 7: 41}
     print("hej")
 
 def motorscore_impairments_setvalue() -> float:
